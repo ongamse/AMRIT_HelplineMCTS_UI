924d52265df757aae88baeb9e2ef0fdc164e946b
swetatech24 <sweta.prakash@wipro.com>
2023-09-14 11:56:20 +0530

    Use of externally-controlled format string

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -208,8 +208,8 @@ export class dashboardContentClass implements OnInit {
   }
 
   handleEvent() {
-    const sanitizedEventSpiltData = String(this.eventSpiltData);
-    console.log(sanitizedEventSpiltData, "in dashboard");
+    const logMessage = `${String(this.eventSpiltData)} in dashboard`;
+console.log(logMessage);
     if (this.eventSpiltData[0] == "Accept" && this.eventSpiltData[2] != undefined && this.eventSpiltData[4] != undefined && this.eventSpiltData[4] == '1') {
       console.log("transfer event received, routing to inner page agent and then from their route to call screen");
       this.dataSettingService.onCall.next({
